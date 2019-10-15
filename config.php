03c249023bc2744d1092b933fa84c22e4361fc9e
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2019-10-15 14:27:40 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?
+
+$useragent = "Mozilla/5.0 (Linux; Android 5.1.1; SM-G935FD Build/LMY48Z) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/39.0.0.0 Safari/537.36";
+$token = "74528b87e4a11667e573bd62a98b733326170147";
+$PHPSESSID = "PHPSESSID=vnqoj3pqvahe9hg3k6ra2u5kdl";
+$cfduid = "__cfduid=d9906075a4717f74019083e15ebdd77461571124098";
+
+
